8abe26bad6fd5aa1219ed3e212014f858aa609d5
Carl-Abu'bakar El-amin Igbafe <165786779+Ali-hellcab@users.noreply.github.com>
2026-07-19 10:15:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,9 @@ demo = gr.Interface(
     description="CNN-BiLSTM based phishing URL detector."
 )
 
-demo.launch()
+import os
+
+demo.launch(
+    server_name="0.0.0.0",
+    server_port=int(os.environ.get("PORT", 10000))
+)
